8f8d3b8136fc9c9a2f11efe285bd31a6c704ce24
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-01-29 16:47:16 -0300

    Adiciona mais imagens para background

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
-// Código da lanterna
-let lanterna = document.querySelector(".lanterna")
-let mensagem = document.querySelector(".msg")
-let lanternaLigada = false
+const lanterna = document.querySelector(".lanterna");
+const mensagem = document.querySelector(".msg");
 
-document.addEventListener('keydown', function (event) {
-    if (event.key == 'f' || event.key == 'F') {
-        if (!lanternaLigada) {
-            ligarLanterna();
-            mensagem.style.display = 'none' // remove o aviso de tecla quando a lanterna é ligada pela primeira vez
-        } else {
-            desligarLanterna();
-        }
+let lanternaLigada = false; // Controla o estado da lanterna
+
+document.addEventListener("keydown", function (event) {
+  if (event.key == "f") {
+    if (!lanternaLigada) {
+      ligarLanterna();
+      mensagem.style.display = "none"; // remove o aviso de tecla quando a lanterna é ligada pela primeira vez
+    } else {
+      desligarLanterna();
     }
+  }
 });
 
 function ligarLanterna() {
-    lanternaLigada = true
+  lanternaLigada = true;
 
-    // Adiciona o evento de mouse para simular a lanterna
-    document.addEventListener('mousemove', moverLanterna)
-};
+  // Adiciona o evento de mouse para simular a lanterna
+  document.addEventListener("mousemove", moverLanterna);
+}
 
 function desligarLanterna() {
-    lanternaLigada = false
+  lanternaLigada = false;
 
-    // Remove o evento de mouse quando a lanterna estiver desligada
-    document.removeEventListener('mousemove', moverLanterna)
+  // Remove o evento de mouse quando a lanterna estiver desligada
+  document.removeEventListener("mousemove", moverLanterna);
 
-    // Desliga a lanterna removendo a mascara de recorte
-    lanterna.style.webkitMaskImage = 'none'
-};
+  // Desliga a lanterna removendo a mascara de recorte
+  lanterna.style.webkitMaskImage = "none";
+}
 
 function moverLanterna(event) {
-    // Calcula as coordenadas(posiçao) do cursor do mouse em relaçao a página...
-    let x = event.pageX + "px";
-    let y = event.pageY + "px"; // ... e as formata em uma string com unidade de px(pixels)
+  // Calcula as coordenadas(posiçao) do cursor do mouse em relaçao a página...
+  let x = event.pageX + "px";
+  let y = event.pageY + "px"; // ... e as formata em uma string com unidade de px(pixels)
 
-    // Adiciona a máscara de recorte para simular a lanterna
-    lanterna.style.webkitMaskImage = `radial-gradient(circle at ${x}  ${y}, transparent 130px, black 20%)`;
-
-};
+  // Adiciona a máscara de recorte para simular a lanterna
+  lanterna.style.webkitMaskImage = `radial-gradient(circle at ${x}  ${y}, transparent 130px, black 20%)`;
+}
 
 // Código que escolhe um background aléatorio
-
-// Caminho das imagens
 let caminhoDasImagens = [
-    'imagens/fnaf.jpg',
-    'imagens/paisagem.jpg',
-    'imagens/navios.jpg',
-    'imagens/corredor.jpg',
-    'imagens/corredor2.jpg',
-    'imagens/corredor3.jpeg'
-];
+  "imagens/corredor.jpg",
+  "imagens/corredor2.jpg",
+  "imagens/corredor3.jpeg",
+  "imagens/fnaf.jpg",
+  "imagens/navios.jpg",
+  "imagens/paisagem.jpg",
+]; // Caminho das imagens
 
 //Escolhe aleatoriamente um caminho
-let caminhoAleatorio = caminhoDasImagens[Math.floor(Math.random() * caminhoDasImagens.length)]
+let caminhoAleatorio = caminhoDasImagens[Math.floor(Math.random() * caminhoDasImagens.length)];
 
 // Atribui o caminho aleatório escolhido para o src da imagem
-let imagemAleatoria = document.getElementById('imagemAleatoria')
-imagemAleatoria.src = caminhoAleatorio
+let imagemAleatoria = document.getElementById("imagemAleatoria");
+imagemAleatoria.src = caminhoAleatorio;
